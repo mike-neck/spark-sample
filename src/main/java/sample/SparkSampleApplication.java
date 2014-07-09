2b6504ffa3d7c546d52a786ba4ccb768befadde6
mike_neck <jkrt3333@gmail.com>
2014-07-09 18:14:44 +0900

    first commit

---
+++ src/main/java/sample/SparkSampleApplication.java
@@ -0,0 +1,41 @@
+package sample;
+
+import spark.SparkBase;
+
+import java.util.Optional;
+
+import static spark.Spark.*;
+
+
+/**
+ * Copyright 2014Shinya Mochida
+ * <p>
+ * Licensed under the Apache License,Version2.0(the"License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing,software
+ * Distributed under the License is distributed on an"AS IS"BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+public class SparkSampleApplication {
+
+    public static void main(String[] args) {
+        Optional<String> optionalPort = Optional.ofNullable(System.getenv("PORT"));
+        optionalPort.ifPresent(p -> {
+            int port = Integer.parseInt(p);
+            setPort(port);
+        });
+        get("/", (req, res) -> "<!DOCTYPE html>" +
+                "<html>" + "<head>" +
+                "<title>" + "spark sample" + "</title>" +
+                "</head>" +
+                "<body>" +
+                "<h1>" + "Hello, World!" + "</h1>" +
+                "</body>" + "</html>");
+    }
+}
